e17fff5acb90f0f7ae1c9c0ffbb4a6ead3c7e447
ChinaraGuliyeva <chinarai@yandex.ru>
2020-05-08 17:31:15 +0400

    added other selects to antd

--- src/ready/Antd.js
+++ src/ready/Antd.js
@@ -11,32 +11,62 @@ class Antd extends Component {
         ],
         france: ["Париж", "Орлеан", "Марсель", "Тулуза"
         ],
-        german: ["Берлин", "Гамбург", "Бремен"],
+        germany: ["Берлин", "Гамбург", "Бремен"],
         spain: ["Мадрид", "Валенсия", "Севилья", "Малага"
         ]
     }
-    changeValue = (event) =>{
-        this.setState({value: event.target.value})
+    changeValue = (event, data) => {
+        console.log(data.value)
+        this.setState({ value: data.value })
     }
-    valueReader = () =>{
-        console.log(this.state.value)
+    valueReader = () => {
+        if (this.state.value == "italy") {
+            return (<Select className="murashki">
+                <Option value={this.state.italy[0]}>{this.state.italy[0]}</Option>
+                <Option value={this.state.italy[1]}>{this.state.italy[1]}</Option>
+                <Option value={this.state.italy[2]}>{this.state.italy[2]}</Option>
+                <Option value={this.state.italy[3]}>{this.state.italy[3]}</Option>
+            </Select>)
+        }
+        if (this.state.value == "france") {
+            return (<Select className="murashki">
+                <Option value={this.state.france[0]}>{this.state.france[0]}</Option>
+                <Option value={this.state.france[1]}>{this.state.france[1]}</Option>
+                <Option value={this.state.france[2]}>{this.state.france[2]}</Option>
+                <Option value={this.state.france[3]}>{this.state.france[3]}</Option>
+            </Select>)
+        }
+        if (this.state.value == "germany") {
+            return (<Select className="murashki">
+                <Option value={this.state.germany[0]}>{this.state.germany[0]}</Option>
+                <Option value={this.state.germany[1]}>{this.state.germany[1]}</Option>
+                <Option value={this.state.germany[2]}>{this.state.germany[2]}</Option>
+            </Select>)
+        }
+        if (this.state.value == "spain") {
+            return (<Select className="murashki">
+                <Option value={this.state.spain[0]}>{this.state.spain[0]}</Option>
+                <Option value={this.state.spain[1]}>{this.state.spain[1]}</Option>
+                <Option value={this.state.spain[2]}>{this.state.spain[2]}</Option>
+                <Option value={this.state.spain[3]}>{this.state.spain[3]}</Option>
+            </Select>)
+        }
+        else {
+            return (''
+            )
+        }
     }
     render() {
         return <div>
-                <Select className="murashki" value={this.state.value} onChange={this.changeValue}>
-                    <Option value="Italy">Италия</Option>
-                    <Option value="France">Франция</Option>
-                    <Option value="Spain">Испания</Option>
-                    <Option value="Germany">Германия</Option>
-                </Select>
-                {this.valueReader()}
-                <Select className="murashki" defaultValue="" >
-                    <Option value=""></Option>
-                    <Option value=""></Option>
-                    <Option value=""></Option>
-                    <Option value=""></Option>
-                </Select>
-                </div>
+            <Select className="murashki" value={this.state.value} onChange={this.changeValue}>
+                <Option value="italy">Италия</Option>
+                <Option value="france">Франция</Option>
+                <Option value="spain">Испания</Option>
+                <Option value="germany">Германия</Option>
+            </Select>
+            {this.valueReader()}
+
+        </div>
     }
 }
 
