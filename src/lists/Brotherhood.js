b4e68772274421b6ec75c5986036e201ebb8152b
ChinaraGuliyeva <chinarai@yandex.ru>
2020-05-10 23:17:33 +0400

    Edited brotherhood component

--- src/lists/Brotherhood.js
+++ src/lists/Brotherhood.js
@@ -2,44 +2,31 @@ import React, { Component } from 'react';
 
 class Brotherhood extends Component {
     state = {
-        data: []
+        data: [],
+        value: ""
     }
-    componentDidMount() {
-        this.getData();
+    valueHandler = (event) => {
+        this.setState({ value: event.target.value })
+        //console.log(event.target.value);
+        setTimeout(this.findName, 10000);
     }
     getData = () => {
         fetch('https://5ea5ca472d86f00016b4626d.mockapi.io/brotherhood')
             .then(responce => responce.json())
             .then(data => this.setState({ data: data }));
     }
-    render() {
-        if (this.state.data.length>0){
-        return <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Department</th>
-                    <th>Role</th>
-                </tr>
-            </thead>
-            <tbody>
-                {this.state.data.map(user => {
-                    return (
-                        <tr key={user.id}>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.department}</td>
-                            <td>{user.role}</td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>}
-        else {
-            return <div className="load">Падажды</div>
-        }
-
+    findName = ()=>{
+        let result = this.state.data.find(element => element.name==this.state.value);
+        console.log(result);
+    }
+    componentDidMount() {
+        this.getData();
+    }
+    render(){
+        return (
+        <div className="searchName">
+            <label for="search">Поиск</label><input value={this.state.value} id="search" onChange={this.valueHandler} />
+        </div>)
     }
 
 }
